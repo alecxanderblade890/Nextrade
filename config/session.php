6de08da27cf988817fef0d588e1c1b549eaa45d0
alecxanderblade890 <alecxanderandaya6@gmail.com>
2025-07-30 14:35:17 +0800

    Removed some configs from app.php and session.php

--- config/session.php
+++ config/session.php
@@ -156,7 +156,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', '.nextrade-production.up.railway.app'),
+    'domain' => env('SESSION_DOMAIN'),
 
     /*
     |--------------------------------------------------------------------------
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', true),
+    'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
