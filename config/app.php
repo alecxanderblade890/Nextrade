cabf8b78f0402745dc8b27934187e158f956b1b7
alecxanderblade890 <alecxanderandaya6@gmail.com>
2025-07-30 14:59:07 +0800

    Change APP_URL back to localhost for debugging in railway

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://nextrade-production.up.railway.app/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
