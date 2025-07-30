fd6d9ce6a1e70aeeb12b78e9831c744d3b15826e
alecxanderblade890 <alecxanderandaya6@gmail.com>
2025-07-30 14:43:47 +0800

    Removed force app url in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-            URL::forceRootUrl(config('app.url'));
-        }
+            URL::forceScheme('https');        }
     }
 }
